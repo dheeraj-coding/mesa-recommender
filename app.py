3b72b92c3d09bb4f01c1dec54e7c6ea08986d8aa
Dheeraj <31967125+dheeraj-coding@users.noreply.github.com>
2023-04-17 22:30:53 -0700

    Implement dynamic playlist creation logic

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import url_for
 from dotenv import load_dotenv
 import pymongo
 
-load_dotenv() # reads your .env
+load_dotenv()  # reads your .env
 
 # Flask-Login provides user session management for Flask. It handles the common tasks of logging in, logging out, and remembering your users’ sessions
 from flask_login import (
@@ -25,6 +25,7 @@ from flask_login import (
     login_required,
 )
 
+
 # Gets environments variable values
 def get_env_variable(name):
     try:
@@ -32,6 +33,7 @@ def get_env_variable(name):
     except KeyError:
         message = "Expected environment variable '{}' not set.".format(name)
         raise Exception(message)
+
 
 # Copied from database.py file for outputting test user information
 def to_datetime(date):
@@ -46,6 +48,7 @@ def to_datetime(date):
                  / np.timedelta64(1, 's'))
     return datetime.utcfromtimestamp(timestamp)
 
+
 # Granting a user or application access permissions to Spotify data and features (e.g your application needs permission from a user to access their playlists)
 # Spotify (Client Credentials) authorization process requires the following:
 SPOTIFY_CLIENT_ID = get_env_variable("SPOTIFY_CLIENT_ID")
@@ -55,8 +58,7 @@ SPOTIFY_REDIRECT_URL = get_env_variable("SPOTIFY_REDIRECT_URL")
 # What previlege users should give to the app
 # The app can make the following requests on behalf of the user
 # Spotify API docs for other possible scopes -- Need to update scoprd
-SPOTIFY_SCOPES = "user-read-private user-read-email user-read-playback-state user-read-currently-playing user-read-playback-position user-modify-playback-state app-remote-control streaming"
-
+SPOTIFY_SCOPES = "user-read-private user-read-email user-read-playback-state user-read-currently-playing user-read-playback-position user-modify-playback-state app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private ugc-image-upload"
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -73,11 +75,11 @@ class UserDBObject:
         obj = super().__new__(cls)
         obj._dict = d
         for k, v in d.items():
-            #print (k, v)
+            # print (k, v)
             setattr(obj, k, v)
-        obj.is_active = True # Remove this later and maintain in DB
+        obj.is_active = True  # Remove this later and maintain in DB
         return obj
-    
+
     def to_dict(self):
         return self._dict
 
@@ -88,7 +90,6 @@ class UserDBObject:
     @property
     def id(self):
         return self._id
-    
 
     def __str__(self):
         return f"User({self.id}, auth={self.is_authenticated})"
@@ -101,22 +102,22 @@ class User:
         self.client = pymongo.MongoClient(get_env_variable("MONGO_URI"))
         self.db = self.client['users']['user_history']
         # self.db.delete_many({})
-        
-    def get_user_by_id(self,user_id):
+
+    def get_user_by_id(self, user_id):
         d = self.db.find_one({"_id": user_id})
         if not d:
             return None
         return UserDBObject(d)
-    
+
     def update_user(self, _id, data):
         result = self.db.update_one(
             {'_id': _id},
             {'$setOnInsert': data},
             upsert=True
         )
-        print (result)
+        print(result)
         return self.get_user_by_id(_id)
-    
+
     def add_song(self, _id, song_id):
         nowTime = np.datetime64(datetime.now()) - (np.timedelta64(0, 'ms') + int(np.floor(np.random.rand() * 500)))
         self.db.update_one({'_id': _id},
@@ -132,15 +133,17 @@ class User:
 
 
 def getNow():
-    return to_datetime(np.datetime64(datetime.now()) - (np.timedelta64(0, 'ms') + int(np.floor(np.random.rand() * 500))))
+    return to_datetime(
+        np.datetime64(datetime.now()) - (np.timedelta64(0, 'ms') + int(np.floor(np.random.rand() * 500))))
+
 
 @login_manager.user_loader
 def load_user(user_id):
     # when user enters the 1st time, this funciton returns None -> sets `current_user` to None
     # if user object is available then `current_user` is set to User object
-    print (user_id)
-    user =  User().get_user_by_id(user_id) # replace this to accomodate all users
-    print (user)
+    print(user_id)
+    user = User().get_user_by_id(user_id)  # replace this to accomodate all users
+    print(user)
     return user
 
 
@@ -160,16 +163,18 @@ def get_spotify_login_link():
     redirect_url.args = data
     return redirect_url
 
+
 def go_to_spotify():
     redirect_url = get_spotify_login_link()
     return redirect(redirect_url.url)
+
 
 # A decorator used to tell the application which URL is associated function
 @app.route("/logout/")
 def logout():
     # set is_authenticated to false in DB
     # Remove current spotify token - placeholder for future code
-    print ("In logout === ", current_user)
+    print("In logout === ", current_user)
     logout_user()
     User().update_user(current_user.id, {"spotify_token": None, "is_authenticated": False})
 
@@ -178,8 +183,8 @@ def logout():
 
 @app.route("/")
 def start():
-    print ("current cuser ", current_user)
-    print (" is authenticated", current_user.is_authenticated)
+    print("current cuser ", current_user)
+    print(" is authenticated", current_user.is_authenticated)
     if not (current_user and current_user.is_authenticated):
         return render_template("hello.html")
     else:
@@ -188,11 +193,10 @@ def start():
         # return redirect("/authorize")
 
 
-
 @app.route("/auth/token")
 def authtoken():
-    print ("current cuser ", current_user)
-    print (" is authenticated", current_user.is_authenticated)
+    print("current cuser ", current_user)
+    print(" is authenticated", current_user.is_authenticated)
     if not (current_user and current_user.is_authenticated):
         # return render_template("hello.html")
         # return json
@@ -212,8 +216,7 @@ def authtoken():
         #     "is_authenticated": True,
         #     "token": current_user.spotify_token,
         # }
-         
-    
+
 
 # A decorator used to tell the application which URL is associated function
 @app.route("/login")
@@ -225,16 +228,16 @@ def login():
         d = {}
         redirect_url = get_spotify_login_link()
         d["spotify_link"] = redirect_url.url
-        print (redirect_url.url)
+        print(redirect_url.url)
         return go_to_spotify()
 
     else:
-        print ("user already authenticated")
+        print("user already authenticated")
         d = get_data(current_user.spotify_token)
 
         # if the user doesn't have a spotify token redirect them to spotify again
         if d == None:
-            print ("redurecting to spotify")
+            print("redurecting to spotify")
             return go_to_spotify()
         else:
             return redirect("https://open.spotify.com/")
@@ -246,12 +249,13 @@ def login():
 def rate():
     track_id = request.form.get("track_id")
     rate = int(request.form.get("rate"))
-    
+
     # track_id rate
     user = User()
 
     user.add_rating(current_user.spotify_id, track_id, rate)
     return 'ok'
+
 
 @app.route("/auth/login")
 def authlogin():
@@ -262,21 +266,22 @@ def authlogin():
         d = {}
         redirect_url = get_spotify_login_link()
         d["spotify_link"] = redirect_url.url
-        print (redirect_url.url)
+        print(redirect_url.url)
         return go_to_spotify()
 
     else:
-        print ("user already authenticated")
+        print("user already authenticated")
         d = get_data(current_user.spotify_token)
 
         # if the user doesn't have a spotify token redirect them to spotify again
         if d == None:
-            print ("redurecting to spotify")
+            print("redurecting to spotify")
             return go_to_spotify()
         else:
             return redirect("https://open.spotify.com/")
 
     return "Something went wrong"
+
 
 # A decorator used to tell the application which URL is associated function
 @app.route("/current/")
@@ -323,6 +328,7 @@ def get_data(spotify_token):
         "track_uri": parsed["item"]["uri"],
     }
 
+
 # A decorator used to tell the application which URL is associated function
 @app.route("/authorize")
 def login_callback():
@@ -330,7 +336,7 @@ def login_callback():
     # There will be error if there is a mismatch between the URLs
     code = request.args.get("code")
     error = request.args.get("error")
-    state = request.args.get("state") # Spotfiy API docs for what are all the possible states
+    state = request.args.get("state")  # Spotfiy API docs for what are all the possible states
     # TODO: handle error cases?
 
     data = {
@@ -340,30 +346,31 @@ def login_callback():
         "client_id": SPOTIFY_CLIENT_ID,
         "client_secret": SPOTIFY_CLIENT_SECRET,
     }
-    url = "https://accounts.spotify.com/api/token" # calling token API with `code`, client_id, client_secret
+    url = "https://accounts.spotify.com/api/token"  # calling token API with `code`, client_id, client_secret
 
     r = requests.post(url, data=data)
     parsed = r.json()
     try:
-        token = parsed["access_token"] # access_token/spotify_token for that specific user. This token should be later used for all user related activites
+        token = parsed[
+            "access_token"]  # access_token/spotify_token for that specific user. This token should be later used for all user related activites
     except KeyError:
-        print ({"data": parsed, "status": "error"})
+        print({"data": parsed, "status": "error"})
         return redirect("/")
 
     # get spotify user id
-    url = "https://api.spotify.com/v1/me" # accessing user profile to get displayname, email and other user indo
+    url = "https://api.spotify.com/v1/me"  # accessing user profile to get displayname, email and other user indo
 
     print("=========url=", url, token)
     headers = {"Authorization": f"Bearer {token}"}
     r = requests.get(url, headers=headers)
 
     # response from Spotify when we ask for user profile
-    print (r.text)
+    print(r.text)
     parsed = r.json()
-    
-    spotify_id = parsed.pop("id") # renaming spotfiy id to id
 
-    data = {"spotify_id": spotify_id, "spotify_token":token, "is_active":True, "is_authenticated": True, **parsed}
+    spotify_id = parsed.pop("id")  # renaming spotfiy id to id
+
+    data = {"spotify_id": spotify_id, "spotify_token": token, "is_active": True, "is_authenticated": True, **parsed}
     user = User()
     # get the UserDBObject for the currently logged in spotify user
     try:
@@ -373,17 +380,15 @@ def login_callback():
 
     # user.add_song(spotify_id, '4AOPgMrdBlaLFF5dxzIhdx')
     # user.add_song(spotify_id, '2eLDUK7EkpENZkDL9O5yhz')
-    #user.add_song(spotify_id, '7mYrw8DN9vDg1c5qqpDboC')
+    # user.add_song(spotify_id, '7mYrw8DN9vDg1c5qqpDboC')
 
-    #user.add_rating(spotify_id, '4AOPgMrdBlaLFF5dxzIhdx', 8)
-    #user.add_rating(spotify_id, '2eLDUK7EkpENZkDL9O5yhz', 7)
-    #user.add_rating(spotify_id,'7mYrw8DN9vDg1c5qqpDboC', 9)
-    
+    # user.add_rating(spotify_id, '4AOPgMrdBlaLFF5dxzIhdx', 8)
+    # user.add_rating(spotify_id, '2eLDUK7EkpENZkDL9O5yhz', 7)
+    # user.add_rating(spotify_id,'7mYrw8DN9vDg1c5qqpDboC', 9)
 
     print("=========login_user==========", curr_user)
 
-
-    login_user(curr_user) # set UserDBObject to the login_manager(flask)
+    login_user(curr_user)  # set UserDBObject to the login_manager(flask)
     print('login_user=======')
     log_info = {
         "user_id": current_user.id,
@@ -393,18 +398,53 @@ def login_callback():
         'token': token
     }
     print('log_info', log_info)
-   
- 
-    #url2 = "https://open.spotify.com/?"
-    #user_homepage = furl(url2)
-    #user_homepage.args = log_info
+
+    # url2 = "https://open.spotify.com/?"
+    # user_homepage = furl(url2)
+    # user_homepage.args = log_info
 
     #  return user.get_user_by_id(spotify_id).to_dict()
-    print ("redirect from authorize")
+    print("redirect from authorize")
     print("token", token)
     data = log_info
+
+    # Code to create playlists for users
+    playlist_ids = check_and_create_playlists(token, curr_user._id)
+    print(playlist_ids)
+
     return render_template("profile.html", name=log_info["user_id"], is_authed=curr_user.is_authenticated, data=data)
-    #return redirect("https://open.spotify.com/?")
+    # return redirect("https://open.spotify.com/?")
+
+
+def check_and_create_playlists(token, user_id):
+    headers = {"Authorization": f"Bearer {token}"}
+    get_playlists = "https://api.spotify.com/v1/me/playlists?limit=50"
+    get_resp = requests.get(get_playlists, headers=headers)
+    get_parsed = get_resp.json()
+    playlists = {}
+    for playlist in get_parsed["items"]:
+        if "MESA" in playlist["name"]:
+            if "Motivation" in playlist["name"]:
+                playlists["motivation"] = playlist["id"]
+            elif "Studying" in playlist["name"]:
+                playlists["studying"] = playlist["id"]
+    if len(playlists) == 0:
+        playlist_create_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+        data_list = {
+            "name": "MESA - Motivation",
+            "description": "A playlist of songs that will motivate you to get up and get moving!",
+            "public": False,
+        }
+        playlist_res = requests.post(playlist_create_url, headers=headers, json=data_list)
+        playlists["motivation"] = playlist_res.json()["id"]
+        data_list = {
+            "name": "MESA - Studying",
+            "description": "A playlist of songs that will motivate you to get up and get moving!",
+            "public": False,
+        }
+        playlist_res = requests.post(playlist_create_url, headers=headers, json=data_list)
+        playlists["studying"] = playlist_res.json()["id"]
+    return playlists
 
 
 if __name__ == "__main__":
